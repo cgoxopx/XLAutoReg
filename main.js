b39efb87f963ed740ebe12a14b25c24492f5bbd5
cgoxopx <cgoxopx@qq.com>
2018-11-03 11:21:57 +0800

    test

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
+(function(){
+
 function autoReg(url , tm){
     var self={
         "url"        : url ,
         "arg"        : [] , 
         "times"     : 999999999 ,
-        "ifr"        : null
+        "ifr"        : null ,
+        "running"   : false;
     };
     
     var counter=0;
@@ -18,6 +21,8 @@ function autoReg(url , tm){
                 return;
             }
         }
+        if(!self.running)
+            return;
         if(counter>=self.times)
             return;
         ++counter;
@@ -25,6 +30,7 @@ function autoReg(url , tm){
     }
     
     self.start=function(){
+        self.running=true;
         self.ifr=document.createElement('iframe');
         self.ifr.onload=self.submiting;
         self.ifr.src=url;
@@ -49,8 +55,7 @@ function autoReg(url , tm){
     }
     
     self.checkPage=function(){
-        if(self.haveTextInput())
-            return true;
+        return self.haveTextInput();
     }
     
     self.setInput=function(){
@@ -128,3 +133,60 @@ function autoReg(url , tm){
     
     return self;
 }
+
+function makeUI(){
+    var div=null;
+    var inputs=[];
+    
+    function makeBegining(){
+        var button=document.createElement('button');
+        button.onclick=makeConfig;
+        button.innerHtml='xlautoreg';
+        document.body.appendChild(button);
+    }
+    
+    function makeConfig(){
+        if(div)
+            return;
+        div=document.createElement('div');
+        document.body.appendChild(div);
+        
+        var button1=document.createElement('button');
+        button1.onclick=startProcess;
+        button1.innerHtml='start';
+        document.body.appendChild(button1);
+        
+        //the function have been canceled
+        //you can reload the page to stop
+        //in addition,you can create multiple processes to register by click start
+        
+        //var button2=document.createElement('button');
+        //button2.onclick=stopProcess;
+        //button2.innerHtml='stop';
+        //document.body.appendChild(button2);
+        
+        var button3=document.createElement('button');
+        button3.onclick=addArgs;
+        button3.innerHtml='add';
+        document.body.appendChild(button3);
+    }
+    
+    function addArgs(){
+        var input=document.createElement('input');
+        input.type='text';
+        div.appendChild(input);
+        inputs.push(input);
+    }
+    
+    function startProcess(){
+        var core=autoReg(window.location.href , 1000);
+        for(var i in inputs){
+            core.arg.push(inputs[i].value);
+        }
+        core.start();
+    }
+}
+
+makeUI();
+
+})();
